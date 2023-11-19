14a6718b46111b2ac7b66988fef7b12fe4691648
Andrey Helldar <helldar@dragon-code.pro>
2023-11-19 19:19:01 +0300

    First Initial

---
+++ README.md
@@ -0,0 +1,33 @@
+# Locale List
+
+![laravel-lang locale list](https://preview.dragon-code.pro/laravel-lang/locale-list.svg?brand=laravel&mode=dark)
+
+[![Stable Version][badge_stable]][link_packagist]
+[![Unstable Version][badge_unstable]][link_packagist]
+[![Total Downloads][badge_downloads]][link_packagist]
+[![License][badge_license]][link_license]
+
+## Documentation
+
+See the [documentation](https://laravel-lang.com) for detailed installation and [usage](https://laravel-lang.com/usage/features/locale-list.html) instructions.
+
+## Contributing
+
+Please see [CONTRIBUTING](https://laravel-lang.com/contributing.html) for details.
+
+## License
+
+This package is licensed under the [MIT License][link_license].
+
+
+[badge_stable]:     https://img.shields.io/github/v/release/Laravel-Lang/locale-list?label=stable&style=flat-square
+
+[badge_unstable]:   https://img.shields.io/badge/unstable-dev--main-orange?style=flat-square
+
+[badge_downloads]:  https://img.shields.io/packagist/dt/Laravel-Lang/locale-list.svg?style=flat-square
+
+[badge_license]:    https://img.shields.io/packagist/l/Laravel-Lang/locale-list.svg?style=flat-square
+
+[link_packagist]:   https://packagist.org/packages/Laravel-Lang/locale-list
+
+[link_license]:     LICENSE
---
+++ src/Locale.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelLang\LocaleList;
+
+use ArchTech\Enums\Values;
+
+enum Locale: string
+{
+    use Values;
+
+    case Afrikaans          = 'af';
+    case Albanian           = 'sq';
+    case Arabic             = 'ar';
+    case Armenian           = 'hy';
+    case Azerbaijani        = 'az';
+    case Basque             = 'eu';
+    case Belarusian         = 'be';
+    case Bengali            = 'bn';
+    case Bosnian            = 'bs';
+    case Bulgarian          = 'bg';
+    case Catalan            = 'ca';
+    case CentralKhmer       = 'km';
+    case Chinese            = 'zh_CN';
+    case ChineseHongKong    = 'zh_HK';
+    case ChineseT           = 'zh_TW';
+    case Croatian           = 'hr';
+    case Czech              = 'cs';
+    case Danish             = 'da';
+    case Dutch              = 'nl';
+    case English            = 'en';
+    case Estonian           = 'et';
+    case Finnish            = 'fi';
+    case French             = 'fr';
+    case Galician           = 'gl';
+    case Georgian           = 'ka';
+    case German             = 'de';
+    case GermanSwitzerland  = 'de_CH';
+    case Greek              = 'el';
+    case Gujarati           = 'gu';
+    case Hebrew             = 'he';
+    case Hindi              = 'hi';
+    case Hungarian          = 'hu';
+    case Icelandic          = 'is';
+    case Indonesian         = 'id';
+    case Italian            = 'it';
+    case Japanese           = 'ja';
+    case Kannada            = 'kn';
+    case Kazakh             = 'kk';
+    case Korean             = 'ko';
+    case Latvian            = 'lv';
+    case Lithuanian         = 'lt';
+    case Macedonian         = 'mk';
+    case Malay              = 'ms';
+    case Marathi            = 'mr';
+    case Mongolian          = 'mn';
+    case Nepali             = 'ne';
+    case NorwegianBokmal    = 'nb';
+    case NorwegianNynorsk   = 'nn';
+    case Occitan            = 'oc';
+    case Pashto             = 'ps';
+    case Persian            = 'fa';
+    case Pilipino           = 'fil';
+    case Polish             = 'pl';
+    case Portuguese         = 'pt';
+    case PortugueseBrazil   = 'pt_BR';
+    case Romanian           = 'ro';
+    case Russian            = 'ru';
+    case Sardinian          = 'sc';
+    case SerbianCyrillic    = 'sr_Cyrl';
+    case SerbianLatin       = 'sr_Latn';
+    case SerbianMontenegrin = 'sr_Latn_ME';
+    case Sinhala            = 'si';
+    case Slovak             = 'sk';
+    case Slovenian          = 'sl';
+    case Spanish            = 'es';
+    case Swahili            = 'sw';
+    case Swedish            = 'sv';
+    case Tagalog            = 'tl';
+    case Tajik              = 'tg';
+    case Thai               = 'th';
+    case Turkish            = 'tr';
+    case Turkmen            = 'tk';
+    case Uighur             = 'ug';
+    case Ukrainian          = 'uk';
+    case Urdu               = 'ur';
+    case UzbekCyrillic      = 'uz_Cyrl';
+    case UzbekLatin         = 'uz_Latn';
+    case Vietnamese         = 'vi';
+    case Welsh              = 'cy';
+}
